28bf1e1cc597068b75fd5bd8be511774408567b8
Matheus Rebola <matheus.rebola@outlook.com>
2024-04-05 16:33:52 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Crie uma plataforma para processamento e análise de dados em tempo real, com suporte para ingestão de dados em tempo real, processamento distribuído e visualização de resultados em tempo real.
---
+++ mercury-spring/src/test/java/br/org/gestao/mercury/MercuryApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.gestao.mercury;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercuryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
